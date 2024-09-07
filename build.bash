c4e2f1513ce18204d4c7054fef2d8a3f94372a6c
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-09-07 10:12:53 +0700

    Add build flags for OpenBSD

--- build.bash
+++ build.bash
@@ -9,6 +9,10 @@ LDFLAGS="-w -s -X main.Version=${version}"
 CGO_CFLAGS_FREEBSD="-I/usr/local/include -std=gnu99"
 CGO_LDFLAGS_FREEBSD="-L/usr/local/lib -lX11 -lXtst -pthread"
 
+# Go flags for OpenBSD
+CGO_CFLAGS_OPENBSD="-Wno-strict-prototypes -I/usr/X11R6/include -std=gnu99"
+CGO_LDFLAGS_OPENBSD="-L/usr/X11R6/lib -lX11 -lXtst -pthread"
+
 # Functions
 build_linux() {
 	CGO_ENABLED=1 \
@@ -22,11 +26,20 @@ build_freebsd() {
 	go build -o ${ibus_e_name} -ldflags "${LDFLAGS}" -mod=vendor
 }
 
+build_openbsd() {
+	CGO_ENABLED=1 \
+	CGO_CFLAGS=${CGO_CFLAGS_FREEBSD} \
+	CGO_LDFLAGS=${CGO_LDFLAGS_FREEBSD} \
+	go build -o ${ibus_e_name} -ldflags "${LDFLAGS}" -mod=vendor
+}
+
 # Main script
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	build_linux
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
 	build_freebsd
+elif [[ "$OSTYPE" == "openbsd"* ]]; then
+	build_openbsd
 else
 	echo "Operating system is not supported currently!"
 fi
