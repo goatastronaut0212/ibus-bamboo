7659fb95fcef1bd925162ea9d155a47513a8d7d1
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-09-07 10:28:27 +0700

    Add OpenBSD test support

--- test.bash
+++ test.bash
@@ -2,6 +2,10 @@
 CGO_CFLAGS_FREEBSD="-I/usr/local/include -std=gnu99"
 CGO_LDFLAGS_FREEBSD="-L/usr/local/lib -lX11 -lXtst -pthread"
 
+# Go flags for OpenBSD
+CGO_CFLAGS_OPENBSD="-Wno-strict-prototypes -I/usr/X11R6/include -std=gnu99"
+CGO_LDFLAGS_OPENBSD="-L/usr/X11R6/lib -lX11 -lXtst -pthread"
+
 # Functions
 test_linux() {
         CGO_ENABLED=1 \
@@ -14,6 +18,16 @@ test_freebsd() {
         CGO_ENABLED=1 \
         CGO_CFLAGS=${CGO_CFLAGS_FREEBSD} \
         CGO_LDFLAGS=${CGO_LDFLAGS_FREEBSD} \
+        go test . -mod=vendor
+
+	CGO_ENABLED=1 go test ./vendor/github.com/BambooEngine/bamboo-core/. -mod=vendor
+}
+
+
+build_openbsd() {
+	CGO_ENABLED=1 \
+	CGO_CFLAGS=${CGO_CFLAGS_OPENBSD} \
+	CGO_LDFLAGS=${CGO_LDFLAGS_OPENBSD} \
         go test . -mod=vendor
 
 	CGO_ENABLED=1 go test ./vendor/github.com/BambooEngine/bamboo-core/. -mod=vendor
